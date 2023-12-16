ad3bf656d0305b7f4cef317b936361927027308d
Gauravk365kansal <132549852+Gauravk365kansal@users.noreply.github.com>
2023-12-16 01:55:54 -0500

    fix code ,add db.js

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const { MongoClient } = require('mongodb');
-
-// Connection URI
-const uri = 'mongodb://127.0.0.1:27017';
-
-
-// Database Name
-const dbName = 'mydatabase';
-
-// Create a new MongoClient
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-async function createDatabaseAndInsertData() {
-  try {
-    // Connect to the MongoDB server
-    await client.connect();
-
-    // Create a new database (it will be created if it doesn't exist)
-    const db = client.db(dbName);
-
-    // Insert data into a collection (it will be created if it doesn't exist)
-    const collection = db.collection('mycollection');
-    await collection.insertOne({ name: 'John Doe', age: 30 });
-
-    console.log('Data inserted successfully');
-  } finally {
-    // Close the connection when done
-    await client.close();
-  }
-}
-
-// Call the function to create the database and insert data
-createDatabaseAndInsertData();
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send('Hello harry')
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
