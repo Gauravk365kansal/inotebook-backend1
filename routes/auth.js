e08cbce0ce0dbb1a35f296b938f580953b5574d1
Gauravk365kansal <132549852+Gauravk365kansal@users.noreply.github.com>
2023-12-16 06:57:31 -0500

    Creating ThunderClient Collections to Manage Requests

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,38 @@
-
 const express = require('express');
 const User = require('../models/User');
-const user = User
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-//Create a user using POST "api/auth/". Doesn't require auth
-router.post('/',[
-    body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
-    body('email').isEmail().withMessage('Invalid email address'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-], (req,res)=> {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-}).then(user => res.json(user))
-.catch(err=>{console.log(err)
-res.json({error:"Email is already in sytem",message: err.message})});
 
-  
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  // Check whether the user with this email exists already
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+    res.json(user)
+    
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occured");
+  }
 })
 
 module.exports = router
