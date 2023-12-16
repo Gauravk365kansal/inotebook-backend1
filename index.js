eeda4fa6afb718b212d1393302c83eaec8a705d0
Gauravk365kansal <132549852+Gauravk365kansal@users.noreply.github.com>
2023-12-16 02:39:46 -0500

    add models,add routes and change index.js

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello harry')
+
+//Available Routes
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.get('/api/v1/login', (req, res) => {
+  res.send('Hello login')
 })
+
+app.get('/api/v1/signup', (req, res) => {
+  res.send('Hello signup')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
